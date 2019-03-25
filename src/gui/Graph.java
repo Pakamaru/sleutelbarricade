62c94b07af24c3f7225d2a032374c669a0653752
Eva <evavanderscheer@live.nl>
2019-03-25 13:14:41 +0100

    mario staat op alle gras tegels

--- src/gui/Graph.java
+++ src/gui/Graph.java
@@ -5,6 +5,7 @@ import models.tiles.Tile;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Graph extends JComponent{
     private int [][] myGraph = new int[10][10];
@@ -17,7 +18,6 @@ public class Graph extends JComponent{
         this.tiles = field.getTiles();
     }
     public void paintComponent(Graphics g){
-        //g.drawImage(Assets.player, 1000,1000, TILE_WIDTH, TILE_HEIGHT, null);
         for (int i = 0; i<myGraph.length; i++){
             for (int j = 0; j<myGraph[i].length; j++){
                 switch (tiles[i][j].getType()){
@@ -26,8 +26,18 @@ public class Graph extends JComponent{
                     case KEY: g.drawImage(Assets.key, 80*i, 80*j, TILE_WIDTH, TILE_HEIGHT, null);
                         break;
                 }
+                addImage(Assets.grass, Assets.player, 0.5f, 10, 10);
+
             }
         }
+    }
+    private void addImage(BufferedImage buff1, BufferedImage buff2,
+                          float opaque, int x, int y) {
+        Graphics2D g2d = buff1.createGraphics();
+        g2d.setComposite(
+                AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opaque));
+        g2d.drawImage(buff2, x, y, null);
+        g2d.dispose();
     }
 
     public static int getTileWidth() {
