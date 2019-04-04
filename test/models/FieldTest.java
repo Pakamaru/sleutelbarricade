ecbb096875e1844bce1c731f7df2c003195ef58b
Amaru <sanamaru99@gmail.com>
2019-04-04 16:52:46 +0200

    added decision driven unit tests for the class Field

--- test/models/FieldTest.java
+++ test/models/FieldTest.java
@@ -1,36 +1,103 @@
 package models;
 
+import gui.Display;
+import models.levels.Level;
+import models.levels.Level1;
+import models.levels.Level2;
+import models.levels.Level3;
+import models.tiles.*;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class FieldTest {
+    Game game = new Game();
+    Display display = new Display("Game", 1200, 850, game);
+    Field field = new Field(display, 1);
 
     @Test
     public void hitWall() {
+        boolean result = field.hitWall(new SolidWall(0, 0));
+
+        Assert.assertEquals(false, result);
     }
 
     @Test
-    public void hitBarrier() {
+    public void hitBarrier_1() {
+        boolean result = field.hitBarrier(new Barrier(0, 0, 100), 100);
+
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void hitBarrier_2() {
+        boolean result = field.hitBarrier(new Barrier(0, 0, 200), 100);
+
+        Assert.assertEquals(false, result);
     }
 
     @Test
     public void hitKey() {
+        boolean result = field.hitKey(new KeyTile(0, 0, new Key(100)), new Pocket());
+
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void hitTile() {
+        boolean result = field.hitTile(new Tile(0, 0));
+
+        Assert.assertEquals(true, result);
     }
 
     @Test
     public void hitEnd() {
+        boolean result = field.hitEnd(new EndTile(0, 0));
+
+        Assert.assertEquals(true, result);
     }
 
+    @Test
+    public void getNextLevel(){
+        int nextLevel = 2;
+        Object lvl = new Object();
+        Object level2 = new Level2();
+        switch(nextLevel){
+            case 1:
+                lvl = new Level1();
+                break;
+            case 2:
+                lvl = new Level2();
+                break;
+            case 3:
+                lvl = new Level3();
+        }boolean equal = true;
+
+        for(int i = 0; i < ((Level2) lvl).getLevel().length; i++){
+            for(int j = 0; j < ((Level2) lvl).getLevel().length; j++){
+                if(((Level2) lvl).getLevel()[i][j].equals(((Level2) level2).getLevel()[i][j]))
+                    equal = false;
+            }
+        }
+
+        Assert.assertEquals(true, equal);
+    }
     @Test
     public void getTiles() {
-    }
+        Tile[][] result = field.getTiles();
+        Field field2 = new Field(display, 1);
+        boolean equal = true;
 
-    @Test
-    public void totalLevels() {
+        for(int i = 0; i < result.length; i++){
+            for(int j = 0; j < result[i].length; j++){
+                if(result[i][j].equals(field2.getTiles()[i][j]))
+                    equal = false;
+            }
+        }
+
+        Assert.assertEquals(true, equal);
     }
 }
--- test/models/PlayerTest.java
+++ test/models/PlayerTest.java
@@ -7,14 +7,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    Game game = new Game();
+    Display display = new Display("Game", 1200, 850, game);
+    Field field = new Field(display, 1);
+    Player player = new Player(display, field, game, field.getPlayerStart()[0], field.getPlayerStart()[1]);
 
     @Test
     public void can_move() {
-        Game game = new Game();
-        Display display = new Display("Game", 1200, 850, game);
-        Field field = new Field(display, 1);
-        Player player = new Player(display, field, game, field.getPlayerStart()[0], field.getPlayerStart()[1]);
-
         player.move(Direction.DOWN);
         player.move(Direction.DOWN);
         player.move(Direction.DOWN);
@@ -27,11 +26,6 @@ public class PlayerTest {
 
     @Test
     public void can_get_Position() {
-        Game game = new Game();
-        Display display = new Display("Game", 1200, 850, game);
-        Field field = new Field(display, 1);
-        Player player = new Player(display, field, game, field.getPlayerStart()[0], field.getPlayerStart()[1]);
-
         player.move(Direction.RIGHT);
         player.move(Direction.RIGHT);
         player.move(Direction.RIGHT);
@@ -39,16 +33,11 @@ public class PlayerTest {
         player.move(Direction.RIGHT);
         int[] pos = player.getPosition();
         int result = pos[0];
-        Assert.assertEquals(5, result);
+        Assert.assertEquals(9, result);
     }
 
     @Test
     public void can_get_Orientation_1() {
-        Game game = new Game();
-        Display display = new Display("Game", 1200, 850, game);
-        Field field = new Field(display, 1);
-        Player player = new Player(display, field, game, field.getPlayerStart()[0], field.getPlayerStart()[1]);
-
         player.move(Direction.RIGHT);
         Direction result = player.getOrientation();
         Assert.assertEquals(Direction.RIGHT, result);
@@ -56,11 +45,6 @@ public class PlayerTest {
 
     @Test
     public void can_get_Orientation_2() {
-        Game game = new Game();
-        Display display = new Display("Game", 1200, 850, game);
-        Field field = new Field(display, 1);
-        Player player = new Player(display, field, game, field.getPlayerStart()[0], field.getPlayerStart()[1]);
-
         player.move(Direction.UP);
         Direction result = player.getOrientation();
         Assert.assertEquals(Direction.UP, result);
