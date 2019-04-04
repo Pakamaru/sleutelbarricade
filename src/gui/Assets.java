6cfdfc0bc65455f4cc60437bbbd2577b1c3fe940
Eva <evavanderscheer@live.nl>
2019-04-04 14:17:47 +0200

    nieuwe plaatjes

--- src/gui/Assets.java
+++ src/gui/Assets.java
@@ -8,12 +8,12 @@ public class Assets {
     public static BufferedImage grass, key, player, solidWall, door, end;
     public static Clip walk, lock, victory, wall;
     public static void init(){
-        grass = ImageLoader.loadImage("res/tiles/grass2.png");
-        key = ImageLoader.loadImage("res/tiles/key.png");
+        grass = ImageLoader.loadImage("res/tiles/grass.png");
+        key = ImageLoader.loadImage("res/tiles/sleutel.png");
         player = ImageLoader.loadImage("res/tiles/amaruPix.png");
         solidWall = ImageLoader.loadImage("res/tiles/wall.png");
-        door = ImageLoader.loadImage("res/tiles/door.png");
-        end = ImageLoader.loadImage("res/tiles/finish.png");
+        door = ImageLoader.loadImage("res/tiles/deur.png");
+        end = ImageLoader.loadImage("res/tiles/Open.png");
         walk = SoundLoader.loadSound("res/sounds/dirt.wav");
         lock = SoundLoader.loadSound("res/sounds/lock.wav");
         victory = SoundLoader.loadSound("res/sounds/victory.wav");
